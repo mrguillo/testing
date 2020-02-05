58688003d930ef6186e6a2a87c6d85d8abf6d9a3
Guillermo Salcedo <guillosc@gmail.com>
2020-02-04 20:03:16 -0600

    deploy trial

---
+++ client/build/precache-manifest.bda64dc0c5e94a81a6c880a3717efd7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26f981486c7137e73613",
+    "url": "/static/js/main.33e62021.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "26f981486c7137e73613",
+    "url": "/static/css/main.61cb59b2.chunk.css"
+  },
+  {
+    "revision": "f7e71fd1638c2b4a8b6da84dc617e1d6",
+    "url": "/index.html"
+  }
+];
